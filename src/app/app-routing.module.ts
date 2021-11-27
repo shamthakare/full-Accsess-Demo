18f91f48868f5ff584c2ca3408987bbff4234e77
Sham <shamthakare1992@gmail.com>
2021-11-27 18:14:54 +0530

    project creation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) },
+  { path: 'store', loadChildren: () => import('./components/mainbody/product/product.module').then(m => m.ProductModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+//
+// ng g module components/mainbody/product --module app --route store --routing

b8b296e7cdff88ef33349b44060eb472d082b168
Sham <shamthakare1992@gmail.com>
2021-11-27 18:31:00 +0530

    adding header and footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './components/mainbody/about/about.component';
+import { ContactComponent } from './components/mainbody/contact/contact.component';
+import { HomeComponent } from './components/mainbody/home/home.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  // { path: '', pathMatch: 'home' },
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'contact', component: ContactComponent },
   { path: 'auth', loadChildren: () => import('./components/auth/auth.module').then(m => m.AuthModule) },
   { path: 'store', loadChildren: () => import('./components/mainbody/product/product.module').then(m => m.ProductModule) }
 ];
